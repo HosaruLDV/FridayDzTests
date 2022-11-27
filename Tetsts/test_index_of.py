e41cd4f35075839c1a22d793b2bebe2c9ad33d82
Kirill <kirill.potapenkov@mail.ru>
2022-11-28 00:14:17 +0300

    dz

---
+++ Tetsts/test_index_of.py
@@ -0,0 +1,8 @@
+from dafriday.index_of import index_of
+
+
+def test_index_of():
+    assert index_of([2, 7, 3, 2, 4], 2) == 0
+    assert index_of([],1) == -1
+    assert index_of([1,2,3,4], 7) == -1
+    assert index_of([1,2,3,2,5], 2, -3) == 3
